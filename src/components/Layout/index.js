c4289f3a32f7e6497dfcef3bd1d54bab0b8905c3
Jialing Ye <jialingye@Jialings-MacBook-Pro.local>
2023-08-08 12:02:27 -0500

    add portfolio

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import Sidebar from '../Sidebar';
+import './index.scss';
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>index
+    <Sidebar/>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='nav-bar'>
+        <Link className='log'
+    </div>
+  )
+}
+
+export default Sidebar
