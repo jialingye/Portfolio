62cc318afc34a9f195b14cf132379005cc9f2a94
Jialing Ye <jialingye@Jialings-MacBook-Pro.local>
2023-08-08 12:16:21 -0500

    add home

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,21 @@
-import Sidebar from '../Sidebar';
-import './index.scss';
-import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar/'
+import './index.scss'
 
 const Layout = () => {
   return (
-    <div>index
-    <Sidebar/>
+    <div className="App">
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
     </div>
   )
 }
